cbeaea8e70b3541fee93bfced278f793f1ab9296
Alibek Karimov <alibek.karimov@gmail.com>
2025-08-14 15:30:49 +0500

    fix

--- contact.js
+++ contact.js
@@ -7,20 +7,20 @@
 })();
 
 function onSubmitContact(token) {
-    form.append('g-recaptcha-response', token);
-        const form = document.getElementById("contactForm");
-        const formData = new FormData(form);
-        formData.append('g-recaptcha-response', token);
-        const templateProperties = Object.fromEntries(formData.entries());        
-        console.log("form", formData);
-        emailjs.send("service_p3l06sf", "template_zem3rml", templateProperties).then(
-          () => {
-            console.log("SUCCESS!");
-          },
-          (error) => {
-            console.log("FAILED...", error);
-          }
-        );
+  form.append("g-recaptcha-response", token);
+  const form = document.getElementById("contactForm");
+  const formData = new FormData(form);
+  formData.append("g-recaptcha-response", token);
+  const templateProperties = Object.fromEntries(formData.entries());
+  console.log("form", formData);
+  emailjs.send("service_p3l06sf", "template_zem3rml", templateProperties).then(
+    () => {
+      console.log("SUCCESS!");
+    },
+    (error) => {
+      console.log("FAILED...", error);
+    }
+  );
 }
 
 window.addEventListener("DOMContentLoaded", function () {
@@ -29,13 +29,7 @@ window.addEventListener("DOMContentLoaded", function () {
     .addEventListener("submit", function (event) {
       event.preventDefault();
       // Show the reCAPTCHA badge
-      
-        grecaptcha.enterprise.execute(
-          "6Lc01aUrAAAAAAAJGo8eiRCBWu13ncdGCGafkBuf",
-          { action: "submit" }
-        );
-        console.log("reCAPTCHA token:", token);
-        
+      grecaptcha.execute();
       // these IDs from the previous steps
     });
 });
